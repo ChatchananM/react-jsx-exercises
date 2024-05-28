dfe86929ee015fea8c4e32d71742159ee8b2e7f8
ChatchananM <165884301+ChatchananM@users.noreply.github.com>
2024-05-28 23:59:41 +0700

    Second commit

--- exercise_04/App.jsx
+++ exercise_04/App.jsx
@@ -6,7 +6,44 @@ const App = () => {
 	const decreaseTemperature = () => setTemperature((prevTemp) => prevTemp - 1);
 	const increaseTemperature = () => setTemperature((prevTemp) => prevTemp + 1);
 
-	return <>{/* code here */};</>;
+	return (
+		<>
+		<Header temperature={temperature} />
+		<Content temperature={temperature}/>
+		<Footer onDecrease={decreaseTemperature}
+                onIncrease={increaseTemperature}/>
+		</>
+	);
 };
+
+const Header = ({ temperature }) => {
+    return (
+      <div>
+        <button>ON/OFF</button>
+        <span>Current Temperature: {temperature} ℃</span>
+      </div>
+    );
+  };
+
+const Content = ({ temperature }) => {
+    return (
+      <div>
+        <Temperature temperature={temperature} />
+      </div>
+    );
+};
+  
+function Temperature({ temperature }) {
+    return <div>{temperature} ℃</div>;
+}
+
+const Footer=({onDecrease, onIncrease})=> {
+	return (
+	  <div>
+		<button onClick={onDecrease}>Down</button>
+		<button onClick={onIncrease}>Up</button>
+	  </div>
+	);
+  } 
 
 export default App;
