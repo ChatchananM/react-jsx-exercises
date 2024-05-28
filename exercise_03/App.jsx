42f5ce23d79a39524c532b91234d41ae9c1b3768
ChatchananM <165884301+ChatchananM@users.noreply.github.com>
2024-05-28 23:35:47 +0700

    Second commit

--- exercise_03/App.jsx
+++ exercise_03/App.jsx
@@ -3,7 +3,43 @@ import React, { useState } from "react";
 const App = () => {
 	const temperature = 25;
 
-	return <>{/* code here */};</>;
+	return (
+		<>
+		  <Header temperature={temperature} />
+		  <Content temperature={temperature}/>
+		  <Footer />
+		</>
+	  );
 };
+
+const Header =({temperature}) => {
+	return (
+	  <div>
+		<button>ON/OFF</button>
+		<span>Current Temperature: {temperature}℃</span>
+	  </div>	
+	);
+  };
+  
+  const Content=({temperature}) =>{
+	return (
+	  <div>
+		<Temperature temperature={temperature} />
+	  </div>
+	);
+  }
+  
+  function Temperature({temperature}) {
+	return <div>{temperature} ℃</div>;
+  }
+  
+  function Footer() {
+	return (
+	  <div>
+		<button>Down</button>
+		<button>Up</button>
+	  </div>
+	);
+  } 
 
 export default App;
